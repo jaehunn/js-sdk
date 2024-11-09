f60675cd34566bda3b27a0b93758856b577f1c76
Jaehun <qkdwogns98@gmail.com>
2024-11-09 14:48:43 +0900

    install vitest

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { getCount } from '.'
+
+describe('getCount', () => {
+  it('should return 11', () => {
+    expect(getCount()).toBe(11)
+  })
+})
---
+++ vitest.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config'
+
+/**
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ * @type {import("vitest").Config}
+ */
+export default defineConfig({
+    test: {
+        globals: true,
+        passWithNoTests: true,
+        setupFiles: ['./vitest.setup.ts'],
+        environment: 'node',
+        coverage: {
+            provider: 'v8',
+            reporter: ['text'],
+            exclude: [
+                'node_modules',
+                'dist',
+                '**/*.d.ts',
+                '**/*.test.ts',
+                '*.config.mjs',
+            ],
+        },
+        testTimeout: 10000,
+        pool: 'forks',
+        isolate: true,
+    },
+})
